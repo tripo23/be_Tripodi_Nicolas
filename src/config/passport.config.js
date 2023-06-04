f064a5e024c530b4d39fb55ab058203b8aa7c95c
Tripo <tripodi.nicolas@gmail.com>
2023-06-04 11:37:55 -0300

    agrega estrategia de login

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -5,6 +5,7 @@ import userModel from "../dao/models/users.model.js"
 
 const LocalStrategy = local.Strategy;
 const initializePassport = () => {
+
     passport.use('register', new LocalStrategy({
         passReqToCallback:true,usernameField:'email'
     }, async (req,username,password,done) => {
@@ -26,7 +27,28 @@ const initializePassport = () => {
         } catch (error) {
             return done("Error getting user: "+error);
         }
-    }))
+    }));
+
+    passport.use('login', new LocalStrategy({ passReqToCallback:true, usernameField: 'email' }, async (req, email, password, done) => {
+        try {
+            const user = await userModel.findOne({ email: email });
+            if (!user) {
+                req.session.errorMessage = 'Usuario inexistente';
+                return done(null, false);
+            }
+            if (!isValidPassword(user, password)) {
+                req.session.errorMessage = 'Contraseña incorrecta';
+                return done(null, false);
+            } else {
+                req.session.errorMessage = '';
+                delete user.password;
+                return done(null, user);
+            }
+        } catch (error) {
+            return done(error);
+        }
+    }));
+    
 
     passport.serializeUser((user,done) => {
         done(null,user._id);
